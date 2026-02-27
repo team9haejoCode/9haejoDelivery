1dbdef861a0e8befd5e1b49308f1a1de7073d80d
qldo <idzhdl@gmail.com>
2026-02-27 15:38:28 +0900

    refactor : API 명세서 규격에 맞게 PaymentController 응답 포맷 수정

--- src/main/java/com/sparta/_9haejodelivery/dto/response/PaymentResponseDto.java
+++ src/main/java/com/sparta/_9haejodelivery/dto/response/PaymentResponseDto.java
@@ -1,20 +1,30 @@
 package com.sparta._9haejodelivery.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sparta._9haejodelivery.domain.Payment;
 import com.sparta._9haejodelivery.domain.enums.PaymentStatus;
+import java.time.LocalDateTime;
 
 public record PaymentResponseDto(
-        Long paymentId,
+        Long paymentId, 
         Long orderId,
         Long amount,
-        PaymentStatus status
+        PaymentStatus status,
+        
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDateTime createdAt,
+        
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDateTime updatedAt
 ) {
     public static PaymentResponseDto from(Payment payment) {
         return new PaymentResponseDto(
                 payment.getId(),
                 payment.getOrderId(),
                 payment.getAmount(),
-                payment.getStatus()
+                payment.getStatus(),
+                payment.getCreatedAt(),
+                payment.getUpdatedAt()
         );
     }
 }
