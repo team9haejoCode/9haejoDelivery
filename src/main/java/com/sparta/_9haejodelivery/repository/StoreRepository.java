7ad387e99bbba53d213451b9de4a1c45a9cb014d
hayeon <hayaon0603@naver.com>
2026-03-03 15:43:33 +0900

    chore: Product 개발을 위해 Store 리포지토리 임시 생성

---
+++ src/main/java/com/sparta/_9haejodelivery/repository/StoreRepository.java
@@ -0,0 +1,8 @@
+package com.sparta._9haejodelivery.repository;
+
+import com.sparta._9haejodelivery.domain.StoreEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// TODO: product 개발을 위한 임시 파일 추후 변경/삭제
+public interface StoreRepository extends JpaRepository<StoreEntity, Integer> {
+}
