ad01a5208b100cc790323d08f7bb1e64bd11c840
JungAh <70677614+Jungahgo@users.noreply.github.com>
2026-03-04 14:30:23 +0900

    refactor: rename된 User Entity에 맞게 Store 엔티티 수정

--- src/main/java/com/sparta/_9haejodelivery/domain/Store.java
+++ src/main/java/com/sparta/_9haejodelivery/domain/Store.java
@@ -29,7 +29,7 @@ public class Store extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "owner_id")
-    private UserEntity owner;
+    private User owner;
 
     @Column(name = "address", length = 50)
     private String address;
@@ -41,7 +41,7 @@ public class Store extends BaseEntity {
     private Boolean isHide;
 
     @Builder
-    public Store(String storeName, Category category, UserEntity owner, UUID regionId, String address, String description, Boolean isHide) {
+    public Store(String storeName, Category category, User owner, UUID regionId, String address, String description, Boolean isHide) {
         this.storeName = storeName;
         this.category = category;
         this.owner = owner;
