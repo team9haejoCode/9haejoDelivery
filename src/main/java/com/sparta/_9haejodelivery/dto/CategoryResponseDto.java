968598caf7948cfb0bcbfb86a4622fd99f07a341
JungAh <70677614+Jungahgo@users.noreply.github.com>
2026-03-05 15:47:17 +0900

    docs: Category controller, dto swagger 문서 추가

--- src/main/java/com/sparta/_9haejodelivery/dto/CategoryResponseDto.java
+++ src/main/java/com/sparta/_9haejodelivery/dto/CategoryResponseDto.java
@@ -1,14 +1,19 @@
 package com.sparta._9haejodelivery.dto;
 
 import com.sparta._9haejodelivery.domain.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
+@Schema(description = "카테고리 응답")
 public class CategoryResponseDto {
 
+    @Schema(description = "카테고리 ID")
     private final UUID categoryId;
+
+    @Schema(description = "카테고리명", example = "한식")
     private final String categoryName;
 
     public CategoryResponseDto(Category category) {
