296502a8f979f54c16c6e1ff3bb328b2be0706b9
JungAh <70677614+Jungahgo@users.noreply.github.com>
2026-03-05 12:37:26 +0900

    feat: Region entity 생성

---
+++ src/main/java/com/sparta/_9haejodelivery/domain/Region.java
@@ -0,0 +1,43 @@
+package com.sparta._9haejodelivery.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "P_REGION")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Region extends BaseEntity {
+
+    @Id
+    @Column(name = "region_id", length = 10, nullable = false)
+    private String regionId;
+
+    @Column(name = "city", length = 20, nullable = false)
+    private String city;
+
+    @Column(name = "district", length = 20, nullable = false)
+    private String district;
+
+    @Column(name = "neighborhood", length = 20, nullable = false)
+    private String neighborhood;
+
+    @Column(name = "postal_code", length = 10)
+    private String postalCode;
+
+    @Builder
+    public Region(String regionId, String city, String district, String neighborhood, String postalCode) {
+        this.regionId = regionId;
+        this.city = city;
+        this.district = district;
+        this.neighborhood = neighborhood;
+        this.postalCode = postalCode;
+    }
+
+    public void updateRegion(String city, String district, String neighborhood, String postalCode) {
+        if (city != null) this.city = city;
+        if (district != null) this.district = district;
+        if (neighborhood != null) this.neighborhood = neighborhood;
+        if (postalCode != null) this.postalCode = postalCode;
+    }
+}
---
+++ src/main/java/com/sparta/_9haejodelivery/repository/RegionRepository.java
@@ -0,0 +1,10 @@
+package com.sparta._9haejodelivery.repository;
+
+import com.sparta._9haejodelivery.domain.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RegionRepository extends JpaRepository<Region, UUID> {
+
+}
