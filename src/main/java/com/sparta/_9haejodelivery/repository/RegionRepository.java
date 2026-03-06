72cef78c4f9ba25e77473c8b22b60204b97959bc
JungAh <70677614+Jungahgo@users.noreply.github.com>
2026-03-06 12:22:04 +0900

    refactor: Region 엔티티 수정
    - PK를 bcode로 변경
    - zonecode 필드 제거

--- src/main/java/com/sparta/_9haejodelivery/repository/RegionRepository.java
+++ src/main/java/com/sparta/_9haejodelivery/repository/RegionRepository.java
@@ -3,7 +3,5 @@ package com.sparta._9haejodelivery.repository;
 import com.sparta._9haejodelivery.domain.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface RegionRepository extends JpaRepository<Region, UUID> {
+public interface RegionRepository extends JpaRepository<Region, String> {
 }
