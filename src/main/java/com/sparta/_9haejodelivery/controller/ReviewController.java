595f97be5c0c529d69d46db0203e2b3d83967100
jin <yjj1544@gmail.com>
2026-03-09 14:48:07 +0900

    refactor: Controller 반환 타입 : ResposeEntity 적용

--- src/main/java/com/sparta/_9haejodelivery/controller/ReviewController.java
+++ src/main/java/com/sparta/_9haejodelivery/controller/ReviewController.java
@@ -3,18 +3,15 @@ package com.sparta._9haejodelivery.controller;
 import com.sparta._9haejodelivery.common.ApiResponse;
 import com.sparta._9haejodelivery.dto.ReviewCreateRequestDto;
 import com.sparta._9haejodelivery.dto.ReviewResponseDto;
-import com.sparta._9haejodelivery.dto.ReviewSearchRequestDto;
 import com.sparta._9haejodelivery.dto.ReviewUpdateDto;
 import com.sparta._9haejodelivery.service.ReviewService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -34,48 +31,61 @@ public class ReviewController {
     //post /reviews: 생성
     @Operation(summary = "리뷰 작성")
     @PostMapping("/")
-    public ApiResponse<String> createReview(@RequestBody ReviewCreateRequestDto req,
+    public ResponseEntity<ApiResponse<String>> createReview(@RequestBody ReviewCreateRequestDto req,
                                                             @AuthenticationPrincipal UserDetails userDetails){
         String reviewId=reviewService.createReview(req, userDetails.getUsername());
-        return ApiResponse.success(HttpStatus.CREATED ,"리뷰가 작성되었습니다.",reviewId);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success(HttpStatus.CREATED ,"리뷰가 작성되었습니다.",reviewId));
     }
 
     //get /reviews : 조회
     //get /stores/{storeId}/reviews : 매장 별 리뷰 조회  todo:reviews로 합치는 방식으로 우선 시도
-    @Operation(summary = "리뷰 조회",
-            description = "1. reviewId가 있으면 해당 리뷰 상세 조회, " +
-                    "2. 매장 검색 기능 " +
-                    "3. body에 값이 없으면 전체 조회")
-    @GetMapping("/")
-    public ApiResponse<?> getReviews(@ModelAttribute ReviewSearchRequestDto dto,
-                                     @PageableDefault
-                                             (size = 10,
-                                                     sort = "createdAt",
-                                                     direction = Sort.Direction.DESC) Pageable pageable) {
+    @Operation(summary = "리뷰 상세 조회")
+    @GetMapping("/{reviewId}")
+    public ResponseEntity<ApiResponse<ReviewResponseDto>> getReviewByReviewId(@PathVariable String reviewId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK ,"리뷰 조회 성공", reviewService
+                            .findReviewById(UUID
+                                    .fromString(reviewId))));
+    }
+
+    @Operation(summary = "매장별 리뷰 조회")
+    @GetMapping("/reviews/{storeId}")
+    public ResponseEntity<ApiResponse<Slice<ReviewResponseDto>>> getReviewsByStoreId(@PathVariable String storeId,
+                                                                                    @PageableDefault
+                                                                             (size = 10,
+                                                                                     sort = "createdAt",
+                                                                                     direction = Sort.Direction.DESC) Pageable pageable) {
         //pagable 검증 : 10, 30, 50건 외의 값은 10으로 고정
         if(pageable.getPageSize()!=10 && pageable.getPageSize()!=30 && pageable.getPageSize()!=50){
             pageable= PageRequest.of(pageable.getPageNumber(),10,pageable.getSort());
         }
 
-        if (dto.getReviewId() != null) {
-            return ApiResponse.success(HttpStatus.OK ,"리뷰 조회 성공", reviewService
-                            .findReviewById(UUID
-                                    .fromString(dto.getReviewId())));
-        }else if(dto.getStoreId()!=null){
-            return ApiResponse.success(HttpStatus.OK,
-                            "리뷰 조회 성공",
-                            reviewService.findReviewsByStoreId(
-                                    UUID.fromString(dto.getStoreId()),pageable));
-        } else{ //전체 리뷰 조회는 관리자 권한 확인 후 진행
-            return ApiResponse.success(HttpStatus.OK,
-                            "리뷰 조회 성공",
-                            reviewService.findAllReviews(pageable));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK ,"리뷰 조회 성공", reviewService
+                        .findReviewsByStoreId(UUID
+                                .fromString(storeId),pageable)));
+    }
+
+    @Operation(summary = "리뷰 전체 조회")
+    @GetMapping("/reviews")
+    public ResponseEntity<ApiResponse<Page<ReviewResponseDto>>> getAllReviews(@PageableDefault
+                                                                                             (size = 10,
+                                                                                                     sort = "createdAt",
+                                                                                                     direction = Sort.Direction.DESC) Pageable pageable) {
+        //pagable 검증 : 10, 30, 50건 외의 값은 10으로 고정
+        if (pageable.getPageSize() != 10 && pageable.getPageSize() != 30 && pageable.getPageSize() != 50) {
+            pageable = PageRequest.of(pageable.getPageNumber(), 10, pageable.getSort());
         }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK, "리뷰 조회 성공", reviewService
+                        .findAllReviews(pageable)));
     }
 
     @Operation(summary = "작성한 리뷰 조회")
     @GetMapping("/myReviews")
-    public ApiResponse<Slice<ReviewResponseDto>> getMyReviews(@AuthenticationPrincipal UserDetails userDetails,
+    public ResponseEntity<ApiResponse<Slice<ReviewResponseDto>>> getMyReviews(@AuthenticationPrincipal UserDetails userDetails,
                                                               @PageableDefault
                                                                      (size = 10,
                                                                              sort = "createdAt",
@@ -84,32 +94,35 @@ public class ReviewController {
         if(pageable.getPageSize()!=10 && pageable.getPageSize()!=30 && pageable.getPageSize()!=50){
             pageable= PageRequest.of(pageable.getPageNumber(),10,pageable.getSort());
         }
-        return ApiResponse.success(HttpStatus.OK,
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK,
                         "리뷰 조회 성공",
-                        reviewService.findMyReviews(userDetails.getUsername(),pageable));
+                        reviewService.findMyReviews(userDetails.getUsername(),pageable)));
     }
 
     //patch /reviews/{reviewId} : 수정
     @Operation(summary = "리뷰 수정")
     @PatchMapping("/{reviewId}")
-    public ApiResponse<String> updateReview(@PathVariable String reviewId,
+    public ResponseEntity<ApiResponse<String>> updateReview(@PathVariable String reviewId,
                                                             @RequestBody ReviewUpdateDto req,
                                                             @AuthenticationPrincipal UserDetails userDetails)
             throws AccessDeniedException {
         reviewService.updateReview(UUID
                         .fromString(reviewId)
                 , req, userDetails.getUsername());
-        return ApiResponse.success(HttpStatus.OK,
-                        "리뷰가 수정되었습니다.",reviewId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK,
+                        "리뷰가 수정되었습니다.",reviewId));
     }
 
     //delete /reviews/{reviewId} : 소프트 삭제
     @Operation(summary = "리뷰 삭제", description = "소프트 삭제")
     @DeleteMapping("/{reviewId}")
-    public ApiResponse<String> deleteReview(@PathVariable UUID reviewId,
+    public ResponseEntity<ApiResponse<String>> deleteReview(@PathVariable UUID reviewId,
                                                             @AuthenticationPrincipal UserDetails userDetails)
             throws AccessDeniedException {
         reviewService.deleteReview(reviewId,userDetails.getUsername());
-        return ApiResponse.success(HttpStatus.OK,"리뷰가 삭제되었습니다.");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK,"리뷰가 삭제되었습니다."));
     }
 }
