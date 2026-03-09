c8e26b653947ab908d8af40239d6f0b0e16e9777
jin <yjj1544@gmail.com>
2026-03-09 14:31:46 +0900

    refactor: ai controller 반환 타입 수정

--- src/main/java/com/sparta/_9haejodelivery/controller/AiController.java
+++ src/main/java/com/sparta/_9haejodelivery/controller/AiController.java
@@ -1,5 +1,6 @@
 package com.sparta._9haejodelivery.controller;
 
+import com.sparta._9haejodelivery.common.ApiResponse;
 import com.sparta._9haejodelivery.domain.User;
 import com.sparta._9haejodelivery.dto.AiResponseDto;
 import com.sparta._9haejodelivery.service.AiService;
@@ -7,6 +8,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,9 +26,9 @@ public class AiController {
 
     @Operation(summary = "AI 요청 처리", description = "AI 요청을 처리 후 응답 반환.")
     @PostMapping("/")
-    public AiResponseDto aiLog(@AuthenticationPrincipal UserDetails userDetails,
-                               @Valid @RequestBody AiResponseDto requestDto) {
+    public ResponseEntity<ApiResponse<AiResponseDto>> aiLog(@AuthenticationPrincipal UserDetails userDetails,
+                                             @Valid @RequestBody AiResponseDto requestDto) {
         User user=/*userDetails.getUser()*/null;
-        return aiLogService.ask(user, requestDto.text);
+        return ResponseEntity.ok(ApiResponse.success(HttpStatus.CREATED, "Ai 응답이 완료되었습니다.",aiLogService.ask(user, requestDto.text)));
     }
 }
