c16f4dfca1369f4ac562fc336bb3e051bc7a7d35
jin <yjj1544@gmail.com>
2026-03-10 13:25:48 +0900

    fix: gemini 사용을 위해 build.gradle에 의존성 추가 및 관련 코드 수정 test: 단위 테스트 통과 확인 및 api 테스트 통과

--- src/main/java/com/sparta/_9haejodelivery/service/AiService.java
+++ src/main/java/com/sparta/_9haejodelivery/service/AiService.java
@@ -1,34 +1,53 @@
 package com.sparta._9haejodelivery.service;
 
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
 import com.sparta._9haejodelivery.domain.Ai;
 import com.sparta._9haejodelivery.domain.User;
 import com.sparta._9haejodelivery.dto.AiResponseDto;
 import com.sparta._9haejodelivery.repository.AiLogRepository;
+import com.sparta._9haejodelivery.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 @RequiredArgsConstructor
 public class AiService {
     private final AiLogRepository aiLogRepository;
-    private final ChatClient chatClient;
+    private final UserRepository userRepository;
 
-    public AiResponseDto ask(User user, String request){
-        String response = chatClient.prompt()
-                .system("""
+    private final String geminiKey = System.getenv("GOOGLE_AI_STUDIO_KEY");
+    private final String geminiModel = "models/gemini-2.5-flash";
+
+    public AiResponseDto ask(String username, String request){
+        User user=userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+        String prompt = """
                         당신은 고객이 입력한 정보로 해당 제품에 대한 설명을 만들어줍니다. 50자 내로 작성하세요." +
                         내용:
-                        """)
-                .user(request)
-                .call()
-                .content();
+                        """
+                +request;
+
+        GenerateContentResponse response;
+        try (Client client = Client.builder().apiKey(geminiKey).build()) {
+
+            response = client.models.generateContent(
+                    geminiModel,
+                    prompt,
+                    null);
+        }catch (Exception e) {
+            throw new RuntimeException("AI 호출 중 에러 발생: " + e.getMessage());
+        }
+
         String aiLogId=aiLogRepository.save(Ai.builder()
                 .user(user)
                 .requestData(request)
-                .responseData(response).build()).getAiLogId().toString();
+                .responseData(response.text()).build()).getAiLogId().toString();
+
         return AiResponseDto.builder()
                 .aiLogId(aiLogId)
-                .text(response).build();
+                .text(response.text()).build();
     }
 }
