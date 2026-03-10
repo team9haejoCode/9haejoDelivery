97287c70fcf11ec85cc8d76b1c2f43499875d377
jin <yjj1544@gmail.com>
2026-03-10 13:25:48 +0900

    fix: gemini 사용을 위해 build.gradle에 의존성 추가 및 관련 코드 수정 test: 단위 테스트 통과 확인 및 api 테스트 통과

--- src/main/java/com/sparta/_9haejodelivery/controller/AiController.java
+++ src/main/java/com/sparta/_9haejodelivery/controller/AiController.java
@@ -1,8 +1,9 @@
 package com.sparta._9haejodelivery.controller;
 
 import com.sparta._9haejodelivery.common.ApiResponse;
-import com.sparta._9haejodelivery.domain.User;
+import com.sparta._9haejodelivery.dto.AiRequestDto;
 import com.sparta._9haejodelivery.dto.AiResponseDto;
+import com.sparta._9haejodelivery.global.security.UserDetailsImpl;
 import com.sparta._9haejodelivery.service.AiService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +26,9 @@ public class AiController {
 
     @Operation(summary = "AI 요청 처리", description = "AI 요청을 처리 후 응답 반환.")
     @PostMapping("/")
-    public ResponseEntity<ApiResponse<AiResponseDto>> aiLog(@AuthenticationPrincipal UserDetails userDetails,
-                                             @Valid @RequestBody AiResponseDto requestDto) {
-        User user=/*userDetails.getUser()*/null;
-        return ResponseEntity.ok(ApiResponse.success(HttpStatus.CREATED, "Ai 응답이 완료되었습니다.",aiLogService.ask(user, requestDto.text)));
+    public ResponseEntity<ApiResponse<AiResponseDto>> ask(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                            @Valid @RequestBody AiRequestDto requestDto) {
+        AiResponseDto responseDto = aiLogService.ask(/*userDetails.getUsername()*/"owner", requestDto.text);
+        return ResponseEntity.ok(ApiResponse.success(HttpStatus.CREATED, "Ai 응답이 완료되었습니다.",responseDto));
     }
 }
