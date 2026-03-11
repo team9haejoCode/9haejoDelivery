dd5677d39ccc75856aa29d65614515acb9b624ab
JungAh <70677614+Jungahgo@users.noreply.github.com>
2026-03-11 11:14:09 +0900

    refactor: 누락된 Errorcode 추가

--- src/main/java/com/sparta/_9haejodelivery/common/ErrorCode.java
+++ src/main/java/com/sparta/_9haejodelivery/common/ErrorCode.java
@@ -16,6 +16,7 @@ public enum ErrorCode {
     STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "매장을 찾을 수 없습니다."),
     STORE_FILTER_CONFLICT(HttpStatus.BAD_REQUEST, "category와 sigungu는 동시에 사용할 수 없습니다."),
     STORE_CLOSED(HttpStatus.BAD_REQUEST, "매장이 열리지 않았습니다."),
+    STORE_ACCESS_DENIED(HttpStatus.FORBIDDEN, "매장 접근 권한이 없습니다."),
 
     // Category
     CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다."),
